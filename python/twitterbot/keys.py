2bf2da447cb52a01a71005953a423568f33d1bcc
TriangleBear <kakanini31@gmail.com>
2022-05-23 19:21:02 +0800

    Ignoring Keys

--- python/twitterbot/keys.py
+++
@@ -1,7 +0,0 @@
-keys = dict(
-	consumer_key =          '83Up6amYjmotTOOTKBCm8AHtR',
-	consumer_secret =       'wHtG2FvxcEYjuH3ecSb4ZozfGAXijkK72OeDSgPJiCtHoEaySX',
-	access_token =          '1191547431344697349-JoxxWA16vAS8ywj47NDFPjpGI2Nihu',
-	access_token_secret =   'ip0UdEK7J4iKkmFi9qvzZb5TYxaBqQzi9HLvr1mtc5vNs',
-    cat_api =               '9534c2fb-11e1-4490-aba6-574b19dc046e',
-)
